a44f658a49017e9988070effaa3a8f1f3ebef871
Jason Chuang <jchuang@mozilla.com>
2019-02-04 09:53:37 -0500

    Added copy for doorhanger.

--- src/toolbar/toolbar.js
+++ src/toolbar/toolbar.js
@@ -11,11 +11,11 @@ const title = "Ad Analysis for Facebook (U.S. English) - DISABLED";
  **/
 const strLanguages = {
 	"strTitle": "Ad Analysis for Facebook",
-	"strDisabledMessage": "Ad Analysis for Facebook (U.S. English) has been temporarily disabled.",
-	"strReadMore": "Read more.",
+	"strDisabledMessage": "Ad Analysis for Facebook is temporarily disabled. Facebook has blocked this extension, so it will no longer show you how you’re targeted by advertisers. ",
+	"strLearnMore": "Learn more",
 	"strBlogPost": "Mozilla Raises Concerns Over Facebook's Lack of Transparency",
 	"strBrowseMessage": "You may continue to browse the following public datasets.",
-	"strTargetingData": "Targeting data",
+	"strTargetingData": "Targeting data collected by ProPublica",
 	"strElectionData": "Top political advertisers in the United States",
 };
 
@@ -23,7 +23,7 @@ const strLanguages = {
  * @const {Object.<string, string>} Dictionary containing all links in the doorhanger.
  **/
 const aLinks = {
-	"strBlogPost": "https://blog.mozilla.org/blog/2019/01/31/mozilla-raises-concerns-over-facebooks-lack-of-transparency/",
+	"strLearnMore": "https://blog.mozilla.org/blog/2019/01/31/mozilla-raises-concerns-over-facebooks-lack-of-transparency/",
 	"strTargetingData": "https://mozilla.github.io/ad-analysis-for-facebook/info.html#SeeAllTargets",
 	"strElectionData": "https://mozilla.github.io/ad-analysis-for-facebook/info.html#TopAdvertisers",
 };
@@ -33,13 +33,13 @@ document.addEventListener("DOMContentLoaded", function() {
 	document.title = title;
 	for (const key in strLanguages) {
 		const value = strLanguages[key];
-		document.querySelectorAll(`.${key}`).forEach(elem => {
+		document.querySelectorAll(`.str.${key}`).forEach(elem => {
 			elem.innerText = value;
 		});
 	}
 	for (const key in aLinks) {
 		const value = aLinks[key];
-		document.querySelectorAll(`a.${key}`).forEach(elem => {
+		document.querySelectorAll(`a.str.${key}`).forEach(elem => {
 			elem.href = value;
 			elem.rel = "noopener noreferrer";
 			elem.target = "_blank";
