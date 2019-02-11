36bf60ccaef0e3b8d28575a2676862e23a195086
Jason Chuang <jchuang@mozilla.com>
2019-02-11 16:08:40 -0500

    Cleaned up webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -9,87 +9,105 @@ const inputSourceFolder = "src";
 const outputDebugFolder = "build-debug";
 const outputReleaseFolder = "build-release";
 
-const moduleRules = {
-	rules: [{
-		test: /\.js$/,
-		use: [
-			"babel-loader",
-			"eslint-loader"
-		],
-	}, {
-		test: /\.less$/,
-		use: [
-			"style-loader",
-			"css-loader",
-			"less-loader",
-		],
-	}, {
-		test: /\.html$/,
-		use: [
-			"raw-loader",
-		],
-	}, {
-		test: /manifest\.json/,
-		use: [
-			"raw-loader",
-		],
-	}],
+const getModuleRules = () => {
+	return {
+		rules: [{
+			test: /\.js$/,
+			use: [
+				"babel-loader",
+				"eslint-loader"
+			],
+		}, {
+			test: /\.less$/,
+			use: [
+				"style-loader",
+				"css-loader",
+				"less-loader",
+			],
+		}, {
+			test: /\.html$/,
+			use: [
+				"raw-loader",
+			],
+		}, {
+			test: /manifest\.json/,
+			use: [
+				"raw-loader",
+			],
+		}],
+	};
 };
 
-const watchOptions = {
-	aggregateTimeout: 300,
-	poll: 1000,
+const getWatchOptions = () => {
+	return {
+		aggregateTimeout: 300,
+		poll: 1000,
+	};
 };
 
 const generateHtmlFiles = (folder, isProduction = false) => {
-	const minifyHtml = (options) => {
-		return Object.assign(options, {
-			minify: {
-				collapseWhitespace: true,
-				removeComments: true,
-				removeRedundantAttributes: true,
-				removeScriptTypeAttributes: true,
-				removeStyleLinkTypeAttributes: true,
-			},
-		});
-	};
-	const options = {
+	const baseOptions = {
 		template: `${folder}/${folder}.html`,
 		filename: `${folder}/index.html`,
 		inject: "head",
 		chunks: [folder],
 	};
-	return new HtmlWebpackPlugin(isProduction ? minifyHtml(options) : options);
+	const minifyOptions = {
+		collapseWhitespace: true,
+		removeComments: true,
+		removeRedundantAttributes: true,
+		removeScriptTypeAttributes: true,
+		removeStyleLinkTypeAttributes: true,
+	};
+	const options = {};
+	if (isProduction) {
+		Object.assign(options, baseOptions, minifyOptions);
+	} else {
+		Object.assign(options, baseOptions);
+	}
+	return new HtmlWebpackPlugin(options);
 };
 
 const copyAssetFiles = (folder, output, isProduction = false) => {
-	return new CopyWebpackPlugin([{
+	const patterns = [{
 		from: `${folder}/assets/`,
 		to: path.resolve(__dirname, `${output}/${folder}/assets`),
-	}]);
+	}];
+	return new CopyWebpackPlugin(patterns);
 };
 
 const copyManifestJson = (output, isProduction = false) => {
-	return new CopyWebpackPlugin([{
+	const patterns = [{
 		from: "manifest.json",
 		to: path.resolve(__dirname, `${output}`),
-	}]);
+	}];
+	return new CopyWebpackPlugin(patterns);
 };
 
 const generateEnvironmentVariables = (isProduction = false) => {
-	return new webpack.EnvironmentPlugin({
+	const options = {
 		NODE_ENV: isProduction ? "production" : "development",
 		DEBUG: isProduction ? "false" : "true",
-	});
+	};
+	return new webpack.EnvironmentPlugin(options);
 };
 
 const lintStyleFiles = (isProduction = false) => {
-	return new StyleLintWebpackPlugin({
+	const options = {
 		configFile: ".stylelintrc",
 		context: inputSourceFolder,
 		files: "**/*.less",
 		syntax: "less",
-	});
+	};
+	return new StyleLintWebpackPlugin(options);
+};
+
+const getAliases = () => {
+	return {
+		background: path.resolve(__dirname, inputSourceFolder, "background"),
+		common: path.resolve(__dirname, inputSourceFolder, "common"),
+		toolbar: path.resolve(__dirname, inputSourceFolder, "toolbar"),
+	};
 };
 
 module.exports = [{
@@ -104,7 +122,7 @@ module.exports = [{
 		path: path.resolve(__dirname, outputDebugFolder),
 		filename: "[name]/bundle.js",
 	},
-	module: moduleRules,
+	module: getModuleRules(),
 	plugins: [
 		generateHtmlFiles("toolbar"),
 		copyAssetFiles("toolbar", outputDebugFolder),
@@ -113,15 +131,11 @@ module.exports = [{
 		lintStyleFiles(),
 	],
 	resolve: {
-		alias: {
-			toolbar: path.resolve(__dirname, inputSourceFolder, "toolbar"),
-			background: path.resolve(__dirname, inputSourceFolder, "background"),
-			common: path.resolve(__dirname, inputSourceFolder, "common"),
-		},
+		alias: getAliases(),
 	},
 	devtool: "inline-source-map",
 	watch: false,
-	watchOptions: watchOptions,
+	watchOptions: getWatchOptions(),
 }, {
 	name: "release",
 	mode: "production",
@@ -134,7 +148,7 @@ module.exports = [{
 		path: path.resolve(__dirname, outputReleaseFolder),
 		filename: "[name]/bundle.js",
 	},
-	module: moduleRules,
+	module: getModuleRules(),
 	plugins: [
 		generateHtmlFiles("toolbar", true),
 		copyAssetFiles("toolbar", outputReleaseFolder, true),
@@ -143,10 +157,6 @@ module.exports = [{
 		lintStyleFiles(true),
 	],
 	resolve: {
-		alias: {
-			toolbar: path.resolve(__dirname, inputSourceFolder, "toolbar"),
-			background: path.resolve(__dirname, inputSourceFolder, "background"),
-			common: path.resolve(__dirname, inputSourceFolder, "common"),
-		},
+		alias: getAliases(),
 	},
 }];
